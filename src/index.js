d29a4fa85977e873347d6630c97debca99076d0a
Timur Uzel <timur@Timurs-MacBook-Pro.local>
2017-01-31 09:54:06 -0500

    class selection...

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import cuuid from 'cuuid'
 
 export default function(selector, node, style) {
   const className = 'a' + cuuid()
-  jss.set(`#${className}:${selector}`, style)
+  jss.set(`.${className}:${selector}`, style)
   node.classList.add(className)
 }
