2e2234eac7bac811c6637d46eb08af68ccdd9da9
Timur Uzel <timur@Timurs-MacBook-Pro.local>
2017-01-31 09:16:08 -0500

    forgot class period

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import cuuid from 'cuuid'
 
 export default function(selector, node, style) {
   const className = cuuid()
-  jss.set(`${className}:${selector}`, style)
+  jss.set(`.${className}:${selector}`, style)
   node.classList.add(className)
   return className
 }
